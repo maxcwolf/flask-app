ab1a5e6781c0abaebb804ae1c496c5f3278028e9
Max Wolf <maxcharleswolf@gmail.com>
2019-03-10 13:19:51 -0500

    added templating :paper:

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Whoa dude!"
+    user = {'username': 'RangDipkin'}
+    posts = [
+        {
+            'author': {'username': 'Max'},
+            'body': 'This blog is dumb.'
+        },
+        {
+            'author': {'username': 'Rang'},
+            'body': 'No you\'re dumb!'
+        }
+    ]
+    return render_template('index.html', title='Flask Blog', user=user, posts=posts)
